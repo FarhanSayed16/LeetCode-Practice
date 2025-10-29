b3268b4da5d32cb20ce34cb67e90c1b0cbd637ab
Farhan <farhansayed54@gmail.com>
2025-10-30 03:51:11 +0530

    Add striver.cpp for basic input/output and sum calculation in C++

---
+++ DataStructureswithC/striver.cpp
@@ -0,0 +1,10 @@
+// #include<iostream>
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello, Farhan"<<endl;
+    int a,b;
+    cin>>a>>b;
+    cout<<"Sum: "<<a+b<<endl;
+    return 0;
+}
